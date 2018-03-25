b14faac940a8b08273848dc9b34d24ff45937a31
sylvain <sylvain.laugier.net@gmail.com>
2018-03-25 18:42:59 +0200

    good animation

--- src/AlbumPage/Choices/LoadingChoice.js
+++ src/AlbumPage/Choices/LoadingChoice.js
@@ -15,35 +15,68 @@ const LoadingChoice = () => (
       viewBox="0 0 24 30"
       xmlSpace="preserve"
     >
-      <rect x="0" y="0" width="4" height="10" fill="#333">
-        <animateTransform
-          attributeType="xml"
-          attributeName="transform"
-          type="translate"
-          values="0 0; 0 20; 0 0"
-          begin="0"
+      <rect x="0" y="13" width="4" height="5" fill="#333">
+        <animate
+          attributeName="height"
+          attributeType="XML"
+          values="5;21;5"
+          begin="0s"
+          dur="0.6s"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="y"
+          attributeType="XML"
+          values="13; 5; 13"
+          begin="0s"
           dur="0.6s"
           repeatCount="indefinite"
         />
       </rect>
-      <rect x="10" y="0" width="4" height="10" fill="#333">
-        <animateTransform
-          attributeType="xml"
-          attributeName="transform"
-          type="translate"
-          values="0 0; 0 20; 0 0"
-          begin="0.2s"
+      <rect
+        x="10"
+        y="13"
+        width="4"
+        height="5"
+        fill="#333"
+      >
+        <animate
+          attributeName="height"
+          attributeType="XML"
+          values="5;21;5"
+          begin="0.15s"
+          dur="0.6s"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="y"
+          attributeType="XML"
+          values="13; 5; 13"
+          begin="0.15s"
           dur="0.6s"
           repeatCount="indefinite"
         />
       </rect>
-      <rect x="20" y="0" width="4" height="10" fill="#333">
-        <animateTransform
-          attributeType="xml"
-          attributeName="transform"
-          type="translate"
-          values="0 0; 0 20; 0 0"
-          begin="0.4s"
+      <rect
+        x="20"
+        y="13"
+        width="4"
+        height="5"
+        fill="#333"
+      >
+        <animate
+          attributeName="height"
+          attributeType="XML"
+          values="5;21;5"
+          begin="0.3s"
+          dur="0.6s"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="y"
+          attributeType="XML"
+          values="13; 5; 13"
+          begin="0.3s"
           dur="0.6s"
           repeatCount="indefinite"
         />
