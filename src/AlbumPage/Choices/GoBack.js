a1aebabff9d0006a2f0965fcd981e1e7a94f5f35
sylvain <sylvain.laugier.net@gmail.com>
2018-03-25 18:55:40 +0200

    loading to back button

--- src/AlbumPage/Choices/GoBack.js
+++ src/AlbumPage/Choices/GoBack.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import LoadingChoice from './LoadingChoice';
 
-const GoBack = (props, context) => (
-  props.display ?
-  <div className="back-button-container">
-    <div
-      className="button back-button"
-      onClick={() => (
-        props.setReverseAnim(true, () => context.router.history.goBack())
-      )}
-    >
-      <p>&lsaquo;&ndash;</p>
-    </div>
-  </div> :
-    null
-);
+class GoBack extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+    };
+    this.renderContent = this.renderContent.bind(this);
+  }
+  renderContent() {
+    return this.state.loading ?
+      <LoadingChoice /> :
+      <p>&lsaquo;&ndash;</p>;
+  }
+  render() {
+    return (
+      this.props.display ?
+        <div className="back-button-container">
+          <button
+            className="button back-button"
+            onClick={() => {
+              this.setState({ loading: true });
+              return this.props.setReverseAnim(true, () => this.context.router.history.goBack());
+            }}
+          >
+            {this.renderContent()}
+          </button>
+        </div> :
+        null
+    );
+  }
+}
 
 GoBack.contextTypes = { router: PropTypes.object };
 
 GoBack.propTypes = {
   display: PropTypes.bool.isRequired,
+  setReverseAnim: PropTypes.func.isRequired,
 };
 
 export default GoBack;
