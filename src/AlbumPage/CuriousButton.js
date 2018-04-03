ff879bedb6cb49d9f6610217a6734b1520a32edb
sylvain <sylvain.laugier.net@gmail.com>
2018-04-03 15:14:50 +0200

    adacious css hack for button animation

--- src/AlbumPage/CuriousButton.js
+++ src/AlbumPage/CuriousButton.js
@@ -12,41 +12,45 @@ class CuriousButton extends Component {
     };
     this.renderCuriousButton = this.renderCuriousButton.bind(this);
     this.manageRandomClick = this.manageRandomClick.bind(this);
+    this.isRandomClickAllowed = this.isRandomClickAllowed.bind(this);
   }
-  manageRandomClick() {
+  isRandomClickAllowed() {
     if (!this.state.loading) {
       this.setState({ loading: true }, () => {
-        fetch(`${process.env.REACT_APP_API_URL}/albums/random`, {
-          method: 'GET',
-          headers: new Headers(apiKey),
-        })
-          .then(res => res.json())
-          .then((album) => {
-            const target = album._fields[0].properties._id;
-            const currentId = this.context.router.route.match.params.id;
-            if (currentId !== target) {
-              this.props.setReverseAnim(false, () => this.context.router.history.push(`/${target}`));
-              setTimeout(() => this.setState({ loading: false }), 600);
-            } else {
-              this.setState({ loading: false }, () => this.manageRandomClick());
-            }
-          });
+        setTimeout(this.manageRandomClick, 1000);
       });
     }
   }
+  manageRandomClick() {
+    fetch(`${process.env.REACT_APP_API_URL}/albums/random`, {
+      method: 'GET',
+      headers: new Headers(apiKey),
+    })
+      .then(res => res.json())
+      .then((album) => {
+        const target = album._fields[0].properties._id;
+        const currentId = this.context.router.route.match.params.id;
+        if (currentId !== target) {
+          this.props.setReverseAnim(false, () => this.context.router.history.push(`/${target}`));
+          setTimeout(() => this.setState({ loading: false }), 600);
+        } else {
+          this.manageRandomClick();
+        }
+      });
+  }
   renderCuriousButton() {
     return this.state.loading ?
-      <LoadingChoice /> :
+      <div> <p>Selecting a random album</p><LoadingChoice /></div> :
       <p>I'm feeling curious</p>;
   }
 
   render() {
     return (
-        <div
-          className="random-button button button-album-page"
-          onClick={this.manageRandomClick}
-        >{this.renderCuriousButton()}
-        </div>
+      <div
+        className={this.state.loading ? "curious-loading random-button" : "random-button random-button--unopen button button-album-page"}
+        onClick={this.isRandomClickAllowed}
+      >{this.renderCuriousButton()}
+      </div>
 
     );
   }
